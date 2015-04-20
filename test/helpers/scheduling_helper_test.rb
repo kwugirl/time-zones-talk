2f767fe270dc97c93f2838f68d6ee6121e1092bd
Katherine Wu <kwu@newrelic.com>
2015-04-20 03:44:28 -0400

    Shorten lines of code after having worked on slides

--- test/helpers/scheduling_helper_test.rb
+++ test/helpers/scheduling_helper_test.rb
@@ -2,24 +2,27 @@ require 'test_helper'
 
 class SchedulingHelperTest < ActionView::TestCase
 
+  teardown do
+    Timecop.return
+  end
+
   # attempt #1
   # context "enqueue time" do
   #   setup do
-  #     saturday_pt = DateTime.parse("1th May 2015 10:05:00 AM -07:00")
+  #     saturday_pt = DateTime.parse("10th April 2015 10:05:00 AM -07:00")
   #     Timecop.freeze(saturday_pt)
   #   end
 
   #   should "be the next Monday 1:01am local time of its primary admin (west of UTC)" do
-  #     binding.pry
   #     local_tz = ActiveSupport::TimeZone["Mountain Time (US & Canada)"]
-  #     expected = DateTime.parse("6th Apr 2015 01:01:00 AM -06:00")
+  #     expected = DateTime.parse("12th Apr 2015 01:01:00 AM -06:00")
 
   #     assert_equal expected, local_1am(local_tz)
   #   end
 
   #   should "be the next Monday 1:01am local time of its primary admin (east of UTC)" do
   #     local_tz = ActiveSupport::TimeZone["Bangkok"]
-  #     expected = DateTime.parse("6th Apr 2015 01:01:00 AM +07:00")
+  #     expected = DateTime.parse("12th Apr 2015 01:01:00 AM +07:00")
 
   #     assert_equal expected, local_1am(local_tz)
   #   end
@@ -37,7 +40,7 @@ class SchedulingHelperTest < ActionView::TestCase
         local_tz = ActiveSupport::TimeZone["Mountain Time (US & Canada)"]
 
         expected = DateTime.parse("10th Feb 2014 01:01:00 AM -07:00")
-        assert_equal "Monday", Date::DAYNAMES[expected.wday]
+        assert_wday(expected, "Monday")
 
         assert_equal expected, local_1am(local_tz)
       end
@@ -46,7 +49,7 @@ class SchedulingHelperTest < ActionView::TestCase
         local_tz = ActiveSupport::TimeZone["Bangkok"]
 
         expected = DateTime.parse("10th Feb 2014 01:01:00 AM +07:00")
-        assert_equal "Monday", Date::DAYNAMES[expected.wday]
+        assert_wday(expected, "Monday")
 
         assert_equal expected, local_1am(local_tz)
       end
@@ -62,7 +65,7 @@ class SchedulingHelperTest < ActionView::TestCase
         local_tz = ActiveSupport::TimeZone["Mountain Time (US & Canada)"]
 
         expected = DateTime.parse("9th Jun 2014 01:01:00 AM -06:00")
-        assert_equal "Monday", Date::DAYNAMES[expected.wday]
+        assert_wday(expected, "Monday")
 
         assert_equal expected, local_1am(local_tz)
       end
@@ -71,7 +74,7 @@ class SchedulingHelperTest < ActionView::TestCase
         local_tz = ActiveSupport::TimeZone["Bangkok"]
 
         expected = DateTime.parse("9th Jun 2014 01:01:00 AM +07:00")
-        assert_equal "Monday", Date::DAYNAMES[expected.wday]
+        assert_wday(expected, "Monday")
 
         assert_equal expected, local_1am(local_tz)
       end
@@ -86,15 +89,15 @@ class SchedulingHelperTest < ActionView::TestCase
       should "be the next day when run on late Sunday night" do
         sunday_night = DateTime.parse("1st Jun 2014 11:30:00 PM #{@samoa_tz.formatted_offset}")
         Timecop.freeze(sunday_night)
-        assert_equal "Sunday", Date::DAYNAMES[@samoa_tz.now.wday]
+        assert_wday(@samoa_tz.now, "Sunday")
 
         # It's already Monday in the customer account's time zone before the report runs
-        assert_equal "Monday", Date::DAYNAMES[@wellington_tz.now.wday]
+        assert_wday(@wellington_tz.now, "Monday")
 
         local_tz = ActiveSupport::TimeZone[@wellington_tz.name]
 
         expected = DateTime.parse("2nd Jun 2014 01:01:00 AM +12:00")
-        assert_equal "Monday", Date::DAYNAMES[expected.wday]
+        assert_wday(expected, "Monday")
 
         assert_equal expected, local_1am(local_tz)
       end
@@ -102,12 +105,12 @@ class SchedulingHelperTest < ActionView::TestCase
       should "be the same week when still Monday somewhere" do
         monday = DateTime.parse("2nd Jun 2014 11:30:00 PM #{@samoa_tz.formatted_offset}")
         Timecop.freeze(monday)
-        assert_equal "Monday", Date::DAYNAMES[@samoa_tz.now.wday]
+        assert_wday(monday, "Monday")
 
         local_tz = ActiveSupport::TimeZone[@wellington_tz.name]
 
         expected = DateTime.parse("2nd Jun 2014 01:01:00 AM +12:00")
-        assert_equal "Monday", Date::DAYNAMES[expected.wday]
+        assert_wday(expected, "Monday")
 
         assert_equal expected, local_1am(local_tz)
       end
@@ -115,15 +118,19 @@ class SchedulingHelperTest < ActionView::TestCase
       should "be the next week when run on Tuesday" do
         tuesday = DateTime.parse("3rd Jun 2014 00:30:00 AM #{@samoa_tz.formatted_offset}")
         Timecop.freeze(tuesday)
-        assert_equal "Tuesday", Date::DAYNAMES[@samoa_tz.now.wday]
+        assert_wday(tuesday, "Tuesday")
 
         local_tz = ActiveSupport::TimeZone[@wellington_tz.name]
 
         expected = DateTime.parse("9th Jun 2014 01:01:00 AM +12:00")
-        assert_equal "Monday", Date::DAYNAMES[expected.wday]
+        assert_wday(expected, "Monday")
 
         assert_equal expected, local_1am(local_tz)
       end
     end
+  end
+
+  def assert_wday(expected, day)
+    assert_equal day, Date::DAYNAMES[expected.wday]
   end
 end
